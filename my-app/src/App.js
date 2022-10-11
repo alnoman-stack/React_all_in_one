d886a884ecd5e4cc615756dd01aa1fb3ce42386a
alnoman1995 <alnoman.dit@gmail.com>
2022-10-11 15:58:27 +0600

    useEffect challange complite

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import UseEffect from "./components/useEffect/UseEffect";
+import UseEffectChallange from "./components/useEffect/UseEffectChallange";
 
 const App = () => {
   return (
     <>
-      <UseEffect />
+      <UseEffectChallange />
     </>
   );
 };
---
+++ my-app/src/components/useEffect/UseEffectChallange.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+const UseEffectChallange = () => {
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    document.title = `You click me ${num} times`;
+  });
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Click Me {num}{" "}
+      </button>
+    </>
+  );
+};
+
+export default UseEffectChallange;
