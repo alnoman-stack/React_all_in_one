8a3d1c505a35da6edd342b7e24efb8c337625b8b
alnoman1995 <alnoman.dit@gmail.com>
2022-10-11 15:44:16 +0600

    useEffect hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,12 @@
-import React, { createContext } from "react";
-import ComA from "./components/context API/ComA";
-
-const fName = createContext();
-const lName = createContext();
+import React from "react";
+import UseEffect from "./components/useEffect/UseEffect";
 
 const App = () => {
-  console.log(fName);
   return (
     <>
-      <fName.Provider value={"Al Noman"}>
-        <lName.Provider value={"Hawlader"}>
-          <ComA />
-        </lName.Provider>
-      </fName.Provider>
+      <UseEffect />
     </>
   );
 };
 
 export default App;
-export { fName, lName };
---
+++ my-app/src/components/useEffect/UseEffect.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+const UseEffect = () => {
+  const [num, setNum] = useState(0);
+  const [nums, setNums] = useState(0);
+
+  useEffect(() => {
+    alert("I am clicked");
+  }, [num]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Click me {num}
+      </button>
+
+      <br />
+
+      <button
+        onClick={() => {
+          setNums(nums + 1);
+        }}
+      >
+        Click me {nums}
+      </button>
+    </>
+  );
+};
+
+export default UseEffect;
